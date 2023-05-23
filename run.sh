aeda8c05cced03c92a33f1d0aeb37774e431546f
Hamed Davoodi <109058050+hameddavodi@users.noreply.github.com>
2023-05-23 12:03:24 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,3 +3,8 @@ set -ae
 cd "$(dirname $0)"
 
 uvicorn api.main:app "$@"
+
+
+
+
+// The above code is used to run a local server using batchsrc within any UNIX operating system before starting to use it on VM. 
